959d3846c83b7b5ea465f25149d68f2679f43378
khushiiakter <rkjia1311@gmail.com>
2025-02-21 14:02:52 +0600

    added add task option

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 require("dotenv").config();
 
@@ -11,8 +11,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7xkdi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -21,7 +19,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -33,22 +31,50 @@ async function run() {
     const tasksCollection = client.db("taskify").collection("tasks");
 
     app.post("/users", async (req, res) => {
-        const { _id, email, name, photo } = req.body;
-        const existingUser = await usersCollection.findOne({ _id });
-      
-        if (!existingUser){
-            const result = await usersCollection.insertOne({ _id, email, name, photo});
-            res.send(result);
-        } 
-        
-      });
+      const { _id, email, name, photo } = req.body;
+      const existingUser = await usersCollection.findOne({ _id });
 
+      if (!existingUser) {
+        const result = await usersCollection.insertOne({
+          _id,
+          email,
+          name,
+          photo,
+        });
+        res.send(result);
+      }
+    });
 
+    app.get("/tasks", async (req, res) => {
+      const result = await tasksCollection.find().toArray();
+      res.send(result);
+    });
 
+    app.post("/tasks", async (req, res) => {
+      const newTask = req.body;
 
+      const result = await tasksCollection.insertOne(newTask);
+      res.send(result);
+    });
+    app.put("/tasks/:id", async (req, res) => {
+      const { id } = req.params;
+      const updatedTask = req.body;
+      const result = await tasksCollection.updateOne(
+        { _id: new ObjectId(id) },
+        { $set: updatedTask }
+      );
+      res.send(result);
+    });
+    app.delete("/tasks/:id", async (req, res) => {
+      const { id } = req.params;
+      const result = await tasksCollection.deleteOne({ _id: new ObjectId(id) });
+      res.send(result);
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -56,12 +82,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
 app.get("/", (req, res) => {
-    res.send("Taskify is running");
-  });
-  
-  app.listen(port, () => {
-    console.log(`Taskify is sitting on port ${port}`);
-  });
+  res.send("Taskify is running");
+});
+
+app.listen(port, () => {
+  console.log(`Taskify is sitting on port ${port}`);
+});
